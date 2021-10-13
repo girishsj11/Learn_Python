d0a1ba462486b62e6e473271242ccb853d84b3fe
girishsj <59494641+girishsj11@users.noreply.github.com>
2021-10-13 12:56:10 +0530

    Update daily_coding_641.py

--- Python_Programs/Daily_coding_problems/daily_coding_641.py
+++ Python_Programs/Daily_coding_problems/daily_coding_641.py
@@ -10,3 +10,14 @@ Do this in O(N) time.
 
 '''
 
+def function(l):
+    result = 1 #initial value to be 1
+    for i in range(0,len(l)):
+        if(l[i]<=result):
+            result += l[i]
+        else:
+            break
+    return result
+
+l = [1,1,3,5] #[1,1,1,] [1,2,3,10] [1,2,3,4,5,9]
+print(function(l))
