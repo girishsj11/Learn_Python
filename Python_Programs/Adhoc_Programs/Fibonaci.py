10b8bfd1019a99809b068492a7211162d9bd3c3a
girishsj <59494641+girishsj11@users.noreply.github.com>
2022-02-11 17:43:01 +0530

    Update Fibonaci.py

--- Python_Programs/Adhoc_Programs/Fibonaci.py
+++ Python_Programs/Adhoc_Programs/Fibonaci.py
@@ -1,2 +1,12 @@
-#Generating the fibonaci series for given n number digits
-def fib(num):
+#generating the fibonaci series for n number terms
+
+def fibonaci(num):
+    fibonaci_series = [0,1]
+    for i in range(2,num):
+        fibonaci_series.append(fibonaci_series[i-2]+fibonaci_series[i-1])
+    
+    print(fibonaci_series)
+
+if __name__ == "__main__":
+    fibonaci(5) #output [0, 1, 1, 2, 3]
+    fibonaci(8) #output [0, 1, 1, 2, 3, 5, 8, 13]
