d2bb072a2bdc0041803bb68aa0e3cbc2a1eaac15
girishsj <59494641+girishsj11@users.noreply.github.com>
2022-05-19 12:58:29 +0530

    Update Program_12.py

--- Python_Programs/List/Program_12.py
+++ Python_Programs/List/Program_12.py
@@ -19,6 +19,17 @@ in the array whose sum is 9.
 def main():
   arr = [1,0,2,4,3,5,6]
   target_sum = 6
+  count = 0
+  print("sum of triple elements equals to target sum : ")
+  for i in range(0,len(arr)-2):
+    for j in range(i+1,len(arr)-1):
+      for k in range(j+1,len(arr)):
+        if(target_sum == (arr[i]+arr[j]+arr[k])):
+          count+=1
+          print(arr[i]+","+arr[j]+","+arr[k])
+          
+   print("Number of tripletes are equal to the target_sum : ",count)
+          
   
 
 
