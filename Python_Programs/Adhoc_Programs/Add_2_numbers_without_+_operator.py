6f4bdab9c1f112eebd68a050a30001a4e780681c
girishsj <59494641+girishsj11@users.noreply.github.com>
2022-07-02 20:37:58 +0530

    Update Add_2_numbers_without_+_operator.py

--- Python_Programs/Adhoc_Programs/Add_2_numbers_without_+_operator.py
+++ Python_Programs/Adhoc_Programs/Add_2_numbers_without_+_operator.py
@@ -1,3 +1,14 @@
 #Adding two numbers with using an arithmatic + operator .
 
 def add_two_numbers_without_plus_operator(a,b):
+  data = 0
+  while b!=0:
+    data = a&b
+    a=a^b
+    b=data<<1
+    
+  return a
+
+if __name__ == "__main__":
+  print(add_two_numbers_without_plus_operator(5,10))
+    
