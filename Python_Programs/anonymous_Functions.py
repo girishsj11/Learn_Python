357facabf7807a6364fa62ccbdb573c2cc0ada3f
girishsj <59494641+girishsj11@users.noreply.github.com>
2023-01-07 16:11:52 +0530

    Create anonymous_Functions.py

---
+++ Python_Programs/anonymous_Functions.py
@@ -0,0 +1,34 @@
+'''
+Python Lambda Functions are anonymous function means that the function is without a name. 
+As we already know that the def keyword is used to define a normal function in Python. 
+imilarly, the lambda keyword is used to define an anonymous function in Python
+
+1. lambda
+
+syntax - lambda agruments:expression
+Explanation - A lambda function is a small anonymous function and lambda can take 'n' numbers of arguments , but only one expression .
+
+2. map 
+
+syntax - map(function,iterable)
+Explanation - map() function returns a map object(which is an iterator) of the results after applying the given function to each item of a given iterable (list, tuple etc.).
+
+3. filter 
+
+syntax - filter(function,iterables)
+Explanation - The filter() function returns an iterator were the items are filtered through a function to test if the item is accepted or not.
+
+
+4. reduce
+
+syntax - 
+from functools import reduce 
+reduce(function,iterable)
+
+Explanation - The reduce() function in python performs functional computation by taking a function and an iterable (e.g., list, tuple, dictionary, etc.) 
+as arguments, and the result is returned after computation (the process of applying the function on the iterable).
+The reduce() function in python is defined in the functools module and doesn't return multiple values or any iterator, it just returns a single value as output which is the result of the whole iterable getting reduced to only a single integer or string or boolean.
+'''
+
+
+
