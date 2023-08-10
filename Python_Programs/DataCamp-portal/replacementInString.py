7af2bcd62e97e998c2ee5b8d7a22b847dcb8471a
girishsj <59494641+girishsj11@users.noreply.github.com>
2023-08-10 11:56:48 +0530

    Create replacementInString.py

---
+++ Python_Programs/DataCamp-portal/replacementInString.py
@@ -0,0 +1,11 @@
+#replacementInString
+'''
+Example 1: a user has provided the string “l vey u” and the character “o”, and the output will be “loveyou”.
+
+Example 2: a user has provided the string “D t C mpBl ckFrid yS le” and the character “a”, and the output will be “DataCampBlackFridaySale”.
+'''
+if __name__ == "__main__":
+    string=input("Enter the string : ")
+    org_char=input("Enter the original character to be replaced in the string :")
+    rpl_char=input("Enter character to be placed as a replacement behalf of original one: ")
+    print(s.replace(org_char,rpl_char))
