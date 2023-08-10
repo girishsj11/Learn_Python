12eb7d145dac37fb1add3320c8f1959eb822d648
girishsj <59494641+girishsj11@users.noreply.github.com>
2023-08-10 13:51:34 +0530

    Update PerfectSquare.py

--- Python_Programs/DataCamp-portal/PerfectSquare.py
+++ Python_Programs/DataCamp-portal/PerfectSquare.py
@@ -2,6 +2,11 @@
 
 '''
 Given a positive integer num, write a function that returns True if num is a perfect square else False.
+
+Steps -
+* Finding the square root of the number and converting it into an integer.
+* Applying the square to the square root number and checking if it's a perfect square root.
+* Returning the result as a boolean. 
 '''
 if __name__ == "__main__":
     num = int(input("Enter the number to be verified as perfect square or not : "))
